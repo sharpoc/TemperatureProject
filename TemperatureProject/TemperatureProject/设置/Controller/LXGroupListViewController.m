694b32cddc0a751a34e9c3eccd2f395c1c784440
lvxue <lvxue@01zhuanche.com>
2020-09-08 17:22:53 +0800

    增加添加群组功能

--- TemperatureProject/TemperatureProject/设置/Controller/LXGroupListViewController.m
+++ TemperatureProject/TemperatureProject/设置/Controller/LXGroupListViewController.m
@@ -10,12 +10,14 @@
 #import "LXGroupListViewModel.h"
 #import "LXGroupItemTableViewCell.h"
 #import "LXGroupItemModel.h"
+#import "LXAddGroupViewController.h"
 
 @interface LXGroupListViewController ()<UITableViewDelegate,UITableViewDataSource>
 
 @property (nonatomic,strong) UITableView *groupListTableView;
 @property (nonatomic,strong) LXGroupListViewModel *viewModel;
 @property (nonatomic,copy) NSArray *lists;
+@property (nonatomic,strong) UIButton *joinGroupButton;
 @end
 
 @implementation LXGroupListViewController
@@ -23,7 +25,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    self.view.backgroundColor = KSQColor(70, 182, 151);
+    self.view.backgroundColor = [UIColor whiteColor];
     [self createUI];
     [self createLayout];
     [self createData];
@@ -32,6 +34,7 @@
 - (void)createUI {
     
     [self.view addSubview:self.groupListTableView];
+    [self.view addSubview:self.joinGroupButton];
 }
 
 - (void)createLayout {
@@ -41,8 +44,18 @@
         make.left.mas_equalTo(0);
         make.right.mas_equalTo(0);
         make.top.mas_equalTo(self.navView.mas_bottom);
-        make.bottom.mas_equalTo(0);
+        make.bottom.mas_equalTo(-50-BottomGap);
     }];
+    
+    [self.joinGroupButton mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.width.mas_equalTo(150);
+        make.height.mas_equalTo(40);
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+        make.bottom.mas_equalTo(-10-BottomGap);
+    }];
+    
+    
 }
 
 - (void)createData {
@@ -55,6 +68,12 @@
             [self.groupListTableView reloadData];
         }
     }];
+}
+
+- (void)joinGroupButtonClick {
+    
+    LXAddGroupViewController *addGroupVC = [[LXAddGroupViewController alloc] init];
+    [self.navigationController pushViewController:addGroupVC animated:YES];
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
@@ -93,9 +112,26 @@
         _groupListTableView = [[UITableView alloc] init];
         _groupListTableView.delegate = self;
         _groupListTableView.dataSource = self;
+        _groupListTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     }
     
     return _groupListTableView;
+}
+
+- (UIButton *)joinGroupButton {
+    
+    if (!_joinGroupButton) {
+        
+        _joinGroupButton = [[UIButton alloc] init];
+        _joinGroupButton.backgroundColor = KSQColor(70, 182, 151);
+        [_joinGroupButton setTitle:@"添加群组" forState:UIControlStateNormal];
+        [_joinGroupButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        _joinGroupButton.layer.cornerRadius = 20;
+        _joinGroupButton.layer.masksToBounds = YES;
+        [_joinGroupButton addTarget:self action:@selector(joinGroupButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    return _joinGroupButton;
 }
 
 - (LXGroupListViewModel *)viewModel {
